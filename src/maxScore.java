2249bbe8d5a97a221c40c813a2264524d118afb3
Danish Ranjan <ranjandanish@gmail.com>
2021-05-23 23:24:42 +0530

    Max Score, leetcode 1255

---
+++ src/maxScore.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class maxScore {
+
+	public static int solution(String[] words, int[] farr, int[] score, int idx) {
+	    if (idx == words.length) {
+			return 0;
+		}
+		int ScoreWhenNo = 0 + solution(words, farr, score, idx+1); //word not included
+		
+		int ScoreofWord = 0;
+		String word = words[idx];
+		boolean flag = true;//true if yes call can be made
+		for(int i=0;i<word.length();i++){
+		    char ch = word.charAt(i);
+		    
+		    if(farr[ch-'a'] == 0){
+		        flag = false;
+		    }
+		    
+		    farr[ch-'a']--;
+		    ScoreofWord += score[ch-'a'];
+		}
+		
+		int ScoreWhenYes = 0;
+		if(flag == true){
+		    ScoreWhenYes = ScoreofWord + solution(words, farr, score, idx+1);
+		}
+		
+		for(int i=0;i<word.length();i++){
+		    char ch = word.charAt(i);
+		    farr[ch-'a']++;
+		}
+		
+		
+		
+		return Math.max(ScoreWhenYes,ScoreWhenNo);
+	}
+
+	public static void main(String[] args) {
+
+		Scanner scn = new Scanner(System.in);
+		int nofWords = scn.nextInt();
+		String[] words = new String[nofWords];
+		for(int i = 0 ; i < words.length; i++) {
+			words[i] = scn.next();
+		}
+		int nofLetters = scn.nextInt();
+		char[] letters = new char[nofLetters];
+		for (int i = 0; i < letters.length; i++) {
+			letters[i] = scn.next().charAt(0);
+		}
+		int[] score = new int[26];
+		for (int i = 0; i < score.length; i++) {
+			score[i] = scn.nextInt();
+		}
+		if (words == null || words.length == 0 || letters == null || letters.length == 0 || score == null
+				|| score.length == 0) {
+			System.out.println(0);
+			return;
+		}
+		int[] farr = new int[score.length];
+		for (char ch : letters) {
+			farr[ch - 'a']++;
+		}
+		System.out.println(solution(words, farr, score, 0));
+
+	}
+}
