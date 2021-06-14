414e97a076627a2af144ed24fcac317f760d4fae
Danish Ranjan <ranjandanish@gmail.com>
2021-06-15 00:18:51 +0530

    Queen Combination

---
+++ src/combination2.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class combination2 {
+
+  public static void combinations(boolean[] boxes, int ci, int ti, int lb){
+    
+    if(ci > ti){
+        
+        for(int i=0i<boxes.length;i++){
+            if(boxes[i] == true){
+                System.out.print("i");
+            }else{
+                System.out.print("-");
+            }
+        }
+        System.out.println();
+        
+        return;
+    }
+    
+    for(int b = lb + 1; b<boxes.length;b++){
+        if(boxes[b] == false){
+            boxes[b] = true;
+            combination(boxes, ci+1, ti, b);
+            boxes[b] = false;
+        }
+    }
+    
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int nboxes = Integer.parseInt(br.readLine());
+    int ritems = Integer.parseInt(br.readLine());
+    combinations(new boolean[nboxes], 1, ritems, -1);
+  }
+
+}
